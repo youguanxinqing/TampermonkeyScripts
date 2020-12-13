1847c803d9bfc2507862559ac44917de0ca26e86
youguanxinqing <youguanxinqing@qq.com>
2020-12-13 10:11:37 +0800

    适配极客时间最新样式

--- scripts/GeektimeBlackTheme.js
+++ scripts/GeektimeBlackTheme.js
@@ -12,38 +12,55 @@
 // ==/UserScript==
 
 var backgroundSelector = "#app > div"
-var wordSelector = "#app > div > div > div > div > div > div > div > div > div"
+var wordSelector = "#app > div._1ysv2txS_0 > div._1Q_izgym_0 > div.ibY_sXau_0 > div > div.simplebar-wrapper > div.simplebar-mask > div > div > div > div > div._2SKlnZlt_0 > div._1kh1ihh6_0._2i1ytqT9_0 > div._3TuZWiOJ_0 > div > div"
 var commentSelector = "#app > div > div > div > div > div > ul > li > div > div > div"
+var boldWordSelector = "span[data-slate-object=mark][data-slate-type=bold]"
+var leftTitleSelector1 = ".simplebar-content ._2NgRM2G9_0"
+var leftTitleSelector2 = ".simplebar-content ._3DJrlH2u_0"
 
-waitForKeyElements (backgroundSelector, changeBackground);
-waitForKeyElements (wordSelector, changeWordColor);
-waitForKeyElements (commentSelector, changeCommentColor);
+waitForKeyElements(backgroundSelector, changeBackground);
+waitForKeyElements(wordSelector, changeWordColor);
+waitForKeyElements(commentSelector, changeCommentColor);
+waitForKeyElements(boldWordSelector, changeBoldWordColor);
+waitForKeyElements(leftTitleSelector1, changeLeftColor1);
+waitForKeyElements(leftTitleSelector2, changeLeftColor2);
+
 
 function changeBackground() {
     'use strict';
 
     // Your code here...
-    $("#app > div > div").css({"background": "rgb(45,49,44)", "border": "0px"}) // 右上
-    $("#app > div > div > div").css({"background": "rgb(45,49,44)", "border": "0px"}) // 页眉，正文，左栏背景
-    $("#app > div:first > div:first > div:first").css({"background": "", "border": "0px"}) // 显示 logo
-    $("#app > div > div > div > div").css({"color": "gray", "border": "0px"}) // 左栏边框
+    $("#app > div > div").css({ "background": "rgb(45,49,44)", "border": "0px" }) // 右上
+    $("#app > div > div > div").css({ "background": "rgb(45,49,44)", "border": "0px" }) // 页眉，正文，左栏背景
+    $("#app > div:first > div:first > div:first").css({ "background": "", "border": "0px" }) // 显示 logo
+    $("#app > div > div > div > div").css({ "color": "gray", "border": "0px" }) // 左栏边框
     // $(backgroundSelector).css({"background": "white"})  // 右下（正文块背景）
 }
 
 function changeWordColor() {
-    $(wordSelector).css({"color": "wheat"})  // 正文
-    $("span[data-slate-object=mark]").css({"color": "red"}) // 正文加粗
-    $("#app > div > div > div > div > div > div").css({"color": "wheat"}) // 左栏章节
+    $(wordSelector).css({ "color": "wheat" })  // 正文
+
+    $("").css("color", "wheat")
+    // $("#app > div > div > div > div > div > div > div").css({"color": "wheat"}) // 左栏章节
     // h1 ... h6
     var hArray = new Array("h1", "h2", "h3", "h4", "h5", "h6")
-    hArray.forEach(function(item, i){
-        $(item).css({"color": "wheat"})
+    hArray.forEach(function (item, i) {
+        $(item).css({ "color": "wheat" })
     })
-    // for (var i = 0; i<hArray.length; i++) {
-    //     $(hArray[i]).css({"color": "wheat"})
-    // }
+}
+
+function changeLeftColor1() {
+    $(".simplebar-content ._2NgRM2G9_0").css("color", "wheat")
+}
+
+function changeLeftColor2() {
+    $(".simplebar-content ._3DJrlH2u_0").css("color", "wheat")
 }
 
 function changeCommentColor() {
-    $(commentSelector).css({"color": "wheat"})
+    $(commentSelector).css({ "color": "wheat" })
+}
+
+function changeBoldWordColor() {
+    $(boldWordSelector).css({ "color": "#ce7a12" }) // 正文加粗
 }
